28ffac1554c2c5edca77b19d5e80eac6328f1167
Dhanush940 <dhanushkunamaneni123@gmail.com>
2024-02-17 15:46:48 +0530

    Added constructors of Boat

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,6 +3,15 @@ package com.driver;
 public class Boat implements WaterVehicle{
     String name="";
     int capacity=0;
+
+    public Boat(String name,int capacity){
+        this.name=name;
+        this.capacity=capacity;
+    }
+
+    public Boat(){
+
+    }
     @Override
     public int getVehicleCapacity() {
         return capacity;
