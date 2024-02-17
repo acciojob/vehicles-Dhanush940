6081b52314ecc23170952bd452e0fc3309cdbdce
Dhanush940 <dhanushkunamaneni123@gmail.com>
2024-02-17 15:41:27 +0530

    Added Boat in WaterVehicle file

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,19 +1,6 @@
 package com.driver;
 
 public class Main {
-    public static class Boat implements WaterVehicle{
-        String name="";
-        int capacity=0;
-        @Override
-        public int getVehicleCapacity() {
-            return capacity;
-        }
-
-        @Override
-        public String getVehicleName() {
-            return name;
-        }
-    }
     public static void main(String[] args) {
         F1 mercedes = new F1("mercedes", false);
         mercedes.move(40, 0);
--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,3 +4,18 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
+
+
+ class Boat implements WaterVehicle{
+    String name="";
+    int capacity=0;
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
+}
